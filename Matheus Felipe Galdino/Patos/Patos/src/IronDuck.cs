e4812913d7666f6f8c0fc121db0832c4015b9f4a
MatheusFelips <126251768+MatheusFelips@users.noreply.github.com>
2024-11-01 20:10:50 -0300

    Matheus Felipe Galdino

---
+++ Matheus Felipe Galdino/Patos/Patos/src/IronDuck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patos.src
+{
+    public class IronDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Displaying a Iron Duck");
+        }
+
+        
+    }
+}
---
+++ Matheus Felipe Galdino/Patos/Patos/src/RedHeadDuck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patos.src
+{
+    public class RedHeadDuck : Duck, IQuackable, IFlyable, ISwimminable
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Displaying a RedHead Duck");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("RedHead Duck is Flying ");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Quack, quack!");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("The RedHead duck is swiming");
+        }
+    }
+}
